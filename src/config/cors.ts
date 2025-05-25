987ee179c3f88fd31c63c4ead94841be719f61df
Rahul Sahani <110347707+Rahul-Sahani04@users.noreply.github.com>
2025-05-25 14:10:39 +0530

    feat(cors): trim and clean allowed origins; log the final list of origins

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -7,6 +7,16 @@ const allowedOrigins = process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS
   ? process.env.ANIWATCH_API_CORS_ALLOWED_ORIGINS.split(",")
   : ["http://localhost:4000", "*", "http://localhost:4500", "https://vianime.vercel.app", "https://v-anime.vercel.app"];
 
+  // Remove any empty strings from the array or spaces
+allowedOrigins.forEach((origin, index) => {
+  allowedOrigins[index] = origin.trim();
+  if (allowedOrigins[index] === "") {
+    allowedOrigins.splice(index, 1);
+  }
+});
+
+console.log("CORS allowed origins:", allowedOrigins);
+
 const corsConfig = cors({
   allowMethods: ["GET"],
   maxAge: 600,
